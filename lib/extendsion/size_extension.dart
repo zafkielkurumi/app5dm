2667c26e08d076420eda2d2d22050711f2b38834
kurumi <563693600@qq.com>
2020-03-18 15:26:26 +0800

    重力感应，num_extension
    视频随屏幕

---
+++ lib/extendsion/size_extension.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+extension SizeExtension on num {
+  num get w => ScreenUtil().setWidth(this);
+  num get h => ScreenUtil().setHeight(this);
+  num get sp => ScreenUtil().setSp(this);
+  num get ssp => ScreenUtil().setSp(this, allowFontScalingSelf: false);
+}
